7ae71c28291b1f411e47fb70800ae8e07cabbde1
Semeon Risom <semeon.risom@gmail.com>
2019-05-02 12:24:50 -0500

    Update

--- requirements.bat
+++ requirements.bat
@@ -1,8 +1,5 @@
-#!/bin/bash
-# https://github.com/bndr/pipreqs
-# generate list of required packages
+:: https://github.com/bndr/pipreqs
+:: generate list of required packages
 
-# set path as current location
-cd "$(dirname "$0")"
-# run pipreqs
+:: run pipreqs
 pipreqs --encoding=iso-8859-1 --debug --force --savepath=requirements.txt mdl/
